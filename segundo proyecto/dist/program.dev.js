20b554c8eb095f4c4f76dd73e351b51128ce635b
morerajon <67667692+morerajon@users.noreply.github.com>
2023-02-01 23:28:21 -0300

    Add files via upload

---
+++ segundo proyecto/dist/program.dev.js
@@ -0,0 +1,11 @@
+"use strict";
+
+document.addEventListener("DOMContentLoaded", function () {
+  var abrir = document.getElementById("planeta1");
+  var cerrar = document.getElementById("cerrar");
+  var modalC = document.getElementById("modalc");
+
+  abrir.onclick = function () {
+    modalD.classList.add('show');
+  };
+});
